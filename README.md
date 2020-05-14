ad8b2c9a6fcb87784672a73b1edf27cf723a1126
Sanket Parekh <Sanket.Parekh@McKesson.com>
2020-05-13 17:10:36 -0700

    Make an endpoint of /hello/{name}

--- README.md
+++ README.md
@@ -1,8 +1,17 @@
 # upskilling-homework-05-11-2020
 05/11/2020 Homework as part of upskilling
 
+#### Completed Tasks:
 * Build a Spring Boot app that runs inside of a container.
 When the container is run, the app should start and be a LRP
 * Create an endpoint /coal-mine with a String response of Tweet
 * Run the container on any port but 8080 (Spring default).
 ** Utilize EXPOSE with random port mapping
+* Create an endpoint /hello with a response of { "value" : "Hello World" }
+Update this endpoint to respond with <value>Hello World</value> when my Accept Header is application/xml
+Be sure when the Accept header is */* or application/json it returns the JSON object.
+
+##### build Project
+mvn clean install
+##### Add jar to image
+docker build -t spring-boot .
--- spring-boot/src/main/java/com/example/springboot/HelloController.java
+++ spring-boot/src/main/java/com/example/springboot/HelloController.java
@@ -1,7 +1,7 @@
 package com.example.springboot;
 
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.example.entity.*;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HelloController {
@@ -13,6 +13,24 @@ public class HelloController {
 
     @RequestMapping("/coal-mine")
     public String coalMine() {
-        return "Tweet";
+        String value = "TWEET";
+
+        return value;
+    }
+
+    @RequestMapping("/hello")
+    public Person Hello() {
+        return new Person("Hello World!");
+    }
+
+    @RequestMapping(path = "hello/{name}")
+    public Person getHelloName(@PathVariable("name") String name) {
+        StringBuilder value = new StringBuilder("Hello ");
+
+        if (name != null) {
+            value.append(name);
+        }
+
+        return new Person(value.toString());
     }
 }
